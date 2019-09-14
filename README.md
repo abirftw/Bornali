49619ce647f5611beaf0d905606cf2b46488fd46
shhmrr <murshedshabab@gmail.com>
2019-09-14 21:27:10 +0600

    newBranchShabab

---
+++ README.md
@@ -0,0 +1,6 @@
+Bornali will help teach children with ASD (Autism Spectrum Disorders) by using scientifically backed up teaching methods in order to (i) make it easier for them to express their thoughts, (ii) help them with their social skills & general learning, and (iii) increase their attention span.
+Projected features include:
+(i)Flashcards: Numerous prebuilt flashcards depicting likewise numerous emotions and situations to aid children with ASD to concisely express their thoughts.
+(ii)Puzzles: Interactive puzzles that are proven to induce 'behavior intervention' in children with ASD to help them with their social skills and general learning.
+(iii)Rewards: Virtual collectibles and fun break time activities will be rewarded after completing assigned tasks or puzzles to make the experience more engaging and in turn increase their attention span.
+All of the features will be worked on primarily based on the needs and situation of the children of our country.
---
+++ app/src/main/java/com/sc4ever/bornali/MainActivity.java
@@ -0,0 +1,106 @@
+package com.sc4ever.bornali;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    ImageView imageProfile;
+    Button buttonProfile;
+
+    private static final int PERMISSION_REQUEST = 0;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+        }
+        btn1 = (Button) findViewById(R.id.startbutton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+
+        imageProfile = (ImageView) findViewById(R.id.profilePhoto);
+        buttonProfile = (Button) findViewById(R.id.upload);
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadPhoto();
+            }
+        });
+    }
+
+    void openMenuPage() {
+        System.out.println("button2");
+        Intent intent2 = new Intent(this, PuzzleMenuPage.class);
+        startActivity(intent2);
+    }
+
+    void uploadPhoto() {
+        System.out.println("uploadButton");
+        Intent i = new Intent(Intent.ACTION_PICK);
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        Uri data = Uri.parse(pictureDirectoryPath);
+        i.setDataAndType(data, "image/*");
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK)
+        {
+            if(requestCode == RESULT_LOAD_IMAGE)
+            {
+                Uri imageUri = data.getData();
+                InputStream inputStream;
+
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+                    imageProfile.setImageBitmap(image);
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+                    Toast.makeText(this,"Unable to Open",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }
+    }
+
+}
