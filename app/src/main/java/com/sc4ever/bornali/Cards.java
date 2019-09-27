f2ab6bac0a6b4777a08e99df76991dc5ddc1107a
Asfaqur Rahman <abirftw@gmail.com>
2019-09-27 18:13:52 +0600

    Remove the FAB button for now

--- app/src/main/java/com/sc4ever/bornali/Cards.java
+++ app/src/main/java/com/sc4ever/bornali/Cards.java
@@ -9,6 +9,6 @@ public class Cards extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.card_activity);
+        setContentView(R.layout.activity_cards);
     }
 }
