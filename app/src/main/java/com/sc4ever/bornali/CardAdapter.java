3755e7b692e2832a70de74fdad8b536edb549aba
Asfaqur Rahman <abirftw@gmail.com>
2019-09-29 22:03:54 +0600

    Implement Recyler Card View with Grid Layout for Cue Cards

---
+++ app/src/main/java/com/sc4ever/bornali/CardAdapter.java
@@ -0,0 +1,57 @@
+package com.sc4ever.bornali;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
+
+    private List<CardStyle> cardList;
+    private Context mContext;
+    public CardAdapter(List<CardStyle> cardList, Context mContext) {
+        this.cardList = cardList;
+        this.mContext = mContext;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView cardText;
+        public ImageView cardImage;
+        public ViewHolder(View view){
+            super(view);
+            cardText = view.findViewById(R.id.card_title);
+            cardImage = view.findViewById(R.id.card_image);
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardList.size();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View cardView = inflater.inflate(R.layout.card_row_item,parent,false);
+        return new  ViewHolder(cardView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CardStyle card = cardList.get(position);
+        holder.cardText.setText(card.getCardText());
+        Glide.with(mContext).load(card.getCardImage()).into(holder.cardImage);
+    }
+}
--- app/src/main/java/com/sc4ever/bornali/CardCategoryActivity.java
+++ app/src/main/java/com/sc4ever/bornali/CardCategoryActivity.java
@@ -4,11 +4,37 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class CardCategoryActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardCategoryActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CardAdapter cardAdapter;
+    private List<CardStyle> cardStyleList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+        recyclerView = findViewById(R.id.rv_card_category);
+        cardStyleList = new ArrayList<>();
+        cardAdapter = new CardAdapter(cardStyleList, this);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        //recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(cardAdapter);
+        prepareCards();
+    }
+    private void prepareCards(){
+        cardStyleList.add(new CardStyle(R.drawable.flash23, "Category A"));
+        cardStyleList.add(new CardStyle(R.drawable.logo, "Category B"));
+        cardStyleList.add(new CardStyle(R.drawable.rsz_caretaker, "Category C"));
+        cardStyleList.add(new CardStyle(R.drawable.rsz_sleep, "Category D"));
+
     }
 }
