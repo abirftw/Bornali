47ce9fe8d2272b3aa036d89aca7eb2df74e6694c
shhmrr <murshedshabab@gmail.com>
2019-10-02 03:43:39 +0600

    Initial drag and drop of image for puzzle. with bugs: 1)The size of the image changed. 2)The alignment changed too.

--- app/src/main/java/com/sc4ever/bornali/LevelOne.java
+++ app/src/main/java/com/sc4ever/bornali/LevelOne.java
@@ -7,7 +7,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
 
@@ -17,6 +19,11 @@ public class LevelOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+
+        ViewGroup vv = (ViewGroup) findViewById(R.id.levelOnePage);
+        ImageView img = (ImageView)  vv.findViewById(R.id.mainbird);
+        img.setOnTouchListener(new DragAndDrop(getApplicationContext(), img,vv));
+
         buttonBacktoMenu = (Button) findViewById(R.id.backbuttontoMenu);
         buttonBacktoMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,4 +38,5 @@ public class LevelOne extends AppCompatActivity {
         Intent intentBackk = new Intent(this, PuzzleMenuPage.class);
         startActivity(intentBackk);
     }
+
 }
