d7843ea19659fc074a4c35b275c5ddd138e9ad7e
shhmrr <murshedshabab@gmail.com>
2019-10-15 18:36:45 +0600

    Level five activity added.

---
+++ app/src/main/java/com/sc4ever/bornali/LevelFive.java
@@ -0,0 +1,14 @@
+package com.sc4ever.bornali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelFive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_five);
+    }
+}
