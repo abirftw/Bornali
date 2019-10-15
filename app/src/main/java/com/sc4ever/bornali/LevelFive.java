1c3615c36895397dd44c4c6a7a3b31203501737b
shhmrr <murshedshabab@gmail.com>
2019-10-15 23:25:03 +0600

    Level five. Size and color matching.

--- app/src/main/java/com/sc4ever/bornali/LevelFive.java
+++ app/src/main/java/com/sc4ever/bornali/LevelFive.java
@@ -2,13 +2,106 @@ package com.sc4ever.bornali;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.File;
+
+import static com.sc4ever.bornali.ConvertCoordinate.dpToPx;
 
 public class LevelFive extends AppCompatActivity {
+    private Button buttonBacktoMenu;
+    private Button buttonNextLevel;
+    private ImageView blankImg;
+    private ImageView img;
+    private ImageView levelWinImg;
+    private TextView levelWinText;
+    private ViewGroup vv;
+    private RelativeLayout.LayoutParams layoutParams;
+    private Animation Anim;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_five);
+
+        blankImg = (ImageView)  findViewById(R.id.blueCapLevelFive);
+        blankImg.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = dpToPx(event.getX());
+                int y = dpToPx(event.getY());
+
+                String message = String.format("Coordinates: (%d %d)",x,y);
+                Log.d("ans: ",message);
+                return false;
+            }
+        });
+
+
+        vv = (ViewGroup) findViewById(R.id.levelFivePage);
+        img = (ImageView)  vv.findViewById(R.id.mainCapLevelfive);
+
+        layoutParams = new RelativeLayout.LayoutParams(ConvertCoordinate.dpToPx(100),ConvertCoordinate.dpToPx(100));
+        layoutParams.leftMargin = dpToPx(150);
+        layoutParams.topMargin = dpToPx(300);
+        img.setLayoutParams(layoutParams);
+
+        levelWinImg = (ImageView) findViewById(R.id.levelfivewinimage);
+        levelWinText = (TextView) findViewById(R.id.levelfivewintext);
+
+        img.setOnTouchListener(new View.OnTouchListener() {
+            DragAndDrop dragAndDrop = new DragAndDrop(getApplicationContext(), img,vv,blankImg,layoutParams.leftMargin,layoutParams.topMargin);
+            public boolean onTouch(View v, MotionEvent event) {
+
+                boolean puzzleState = dragAndDrop.onTouch(v, event);
+                if(puzzleState == true)
+                    return true;
+                if(puzzleState == false)
+                {
+                    blankImg.setImageResource(R.drawable.cap);
+                    img.setImageDrawable(null);
+                    levelWinImg.setImageResource(R.drawable.levelwin);
+                    Anim = AnimationUtils.loadAnimation(LevelFive.this,R.anim.levelwinanimtext);
+                    levelWinText.setText(R.string.level_win_text);
+                    levelWinText.setAnimation(Anim);
+
+                    return false;
+                }
+
+                return true;
+            }
+
+        });
+
+        buttonBacktoMenu = (Button) findViewById(R.id.backbuttontoMenu);
+        buttonBacktoMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Back();
+            }
+        });
     }
+
+    void Back() {
+        System.out.println("buttonBacktoMenu");
+        Intent intentBackk = new Intent(this, PuzzleMenuPage.class);
+        startActivity(intentBackk);
+    }
+
+
 }
