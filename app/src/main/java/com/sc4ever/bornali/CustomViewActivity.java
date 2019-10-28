097ee6ed608aba69c094d36fa188acaa7470b5c9
clinton <clintonchakma.24csedu.064@gmail.com>
2019-10-28 23:45:45 +0600

    Drawing puzzle layout has been designed..need to add a few functions

--- app/src/main/java/com/sc4ever/bornali/CustomViewActivity.java
+++ app/src/main/java/com/sc4ever/bornali/CustomViewActivity.java
@@ -3,11 +3,13 @@ package com.sc4ever.bornali ;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class CustomViewActivity extends AppCompatActivity {
 
@@ -15,22 +17,41 @@ public class CustomViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view_acitvity);
-        LinearLayout rootLayout = (LinearLayout)findViewById(R.id.practiceCanvas);
-        final PracticeCanvas practiceCanvas = new PracticeCanvas(this);
-        practiceCanvas.setMinimumWidth(500);
-        practiceCanvas.setMinimumHeight(800);
 
+        listener();
+
+    }
+
+
+
+    private void listener(){
+        RelativeLayout rootLayout = findViewById(R.id.practiceCanvas);
+        final PracticeCanvas practiceCanvas = new PracticeCanvas(this);
         View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+            int index = 0 ;
+
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 practiceCanvas.setCurX(motionEvent.getX());
-                practiceCanvas.setCury(motionEvent.getY());
+                practiceCanvas.setCurY(motionEvent.getY());
+                for(int i = index ; i < practiceCanvas.vitalPoints.size() ; i++){
+                    float x = practiceCanvas.vitalPoints.get(i).getCoordinateX(), y = practiceCanvas.vitalPoints.get(i).getCoordinateY();
+                    if(compare(x,y,motionEvent.getX(),motionEvent.getY())) {
+                        practiceCanvas.updatePath(x, y);
+                        index++ ;
+                    }
+                }
                 practiceCanvas.setBallColor(Color.GREEN);
+                practiceCanvas.print();
                 practiceCanvas.invalidate();
                 return true;
             }
         };
         practiceCanvas.setOnTouchListener(onTouchListener);
         rootLayout.addView(practiceCanvas);
+    }
+
+    private boolean compare (float x1 , float y1 , float x2 , float y2) {
+        return  (x2 >= x1 - 5 && x2 <= x1 + 5 && y2 >= y1 - 5 && y2 <= y1 + 5 );
     }
 }
--- app/src/main/java/com/sc4ever/bornali/PracticeCanvas.java
+++ app/src/main/java/com/sc4ever/bornali/PracticeCanvas.java
@@ -4,70 +4,104 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.constraintlayout.solver.widgets.Rectangle;
 
 import java.util.ArrayList;
-import java.util.List;
-
     public class PracticeCanvas extends View {
-    private float curX = 100 ;
-    private float cury = 100 ;
-    private float dotX , dotY ;
-    private int ballColor = Color.GREEN ;
-    public int getBallColor(){
-        return ballColor ;
-    }
-    public void setBallColor(int ballColor){
-        this.ballColor = ballColor ;
-    }
+        private float curX = 100;
+        private float curY = 100;
+        private int width, height;
+        private int ballColor ;
+        private int index = 0 ; // index of vitalPoints
+        private  float lastXCoordinate , lastYCoordinate ;
+        private ArrayList<dots> alphabetALine1 = new ArrayList<dots>();
+        public ArrayList <dots> pathList = new ArrayList<dots>() ;
+        public ArrayList<dots> vitalPoints = new ArrayList<dots>() ;
 
-    public float getCurX() {
-        return curX;
-    }
+        public void setCurX(float curX) {
+            this.curX = curX;
+        }
+        public void setBallColor(int color){
+            this.ballColor = color ;
+        }
+        public void setCurY(float cury) {
+            this.curY = cury;
+        }
 
-    public void setCurX(float curX) {
-        this.curX = curX;
-    }
+        public PracticeCanvas(Context context) {
+            super(context);
+        }
 
-    public float getCury() {
-        return cury;
-    }
 
-    public void setCury(float cury) {
-        this.cury = cury;
-    }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
 
-    public PracticeCanvas(Context context) {
-        super(context);
-    }
-    private List<dots> alphabets = new ArrayList<>();
-    public void constructAlphabet(){
-        alphabets.add(new dots(100,400));
-        alphabets.add(new dots(90 , 360));
-        alphabets.add(new dots(80,320));
-        alphabets.add(new dots(70,280));
-    }
-    @Override
-    protected void onDraw(Canvas canvas){
-        super.onDraw(canvas);
+            Paint paint = new Paint();
+            paint.setColor(Color.GREEN);
+            paint.setStrokeWidth(20);
+            width = canvas.getWidth() / 10;
+            height = canvas.getHeight() / 10;
 
-        Paint paint = new Paint();
-        paint.setColor(this.getBallColor());
-        canvas.drawCircle(curX,cury,35,paint);
+            canvas.drawCircle(curX, curY, 20, paint); // make the dot initially visible
+            //canvas.drawLine(2*width, 7*height ,5*width,2*height,paint);
+            //canvas.drawLine(5*width,2*height,8*width,7*height,paint);
 
-        canvas.drawCircle(180,900,20,paint);
-        canvas.drawCircle(220,780,20,paint);
-        canvas.drawCircle(290,600,20,paint);
-        canvas.drawCircle(360,400,20,paint);
-        canvas.drawCircle(460,200,20,paint);
-        canvas.drawCircle(540,400,20,paint);
-        canvas.drawCircle(610,600,20,paint);
-        canvas.drawCircle(410,600,20,paint);
-        canvas.drawCircle(500,600,20,paint);
-        canvas.drawCircle(680,780,20,paint);
-        canvas.drawCircle(730,900,20,paint);
+            fillListWithDots(); // prepare arrayList to construct the alphabets
+
+            for (dots i : alphabetALine1) {
+                canvas.drawCircle(i.getCoordinateX(), i.getCoordinateY(), 15, paint);
+            }
+
+            lastXCoordinate = vitalPoints.get(index).getCoordinateX() ;
+            lastYCoordinate = vitalPoints.get(index).getCoordinateY() ;
+
+            for (dots i : pathList){
+                canvas.drawLine(lastXCoordinate,lastYCoordinate , i.getCoordinateX(),i.getCoordinateY(),paint);
+                index++ ;
+            }
+
+        }
+
+
+        private void fillListWithDots() {
+            float j ;
+            for (float i = 2 * width; i <= 8 * width; i += 50) {
+                if(i <= 5*width) {
+                    j = height * 10 - i * 2361 / 1000;
+                    if (i == 2 * width || i >= 5 * width - 49)
+                        vitalPoints.add(new dots(i, j));
+                }
+                else {
+                    j = i * 2361 / 1000 - 9072/10;
+                    if (i >= 8 * width - 49)
+                        vitalPoints.add(new dots(i, j));
+                }
+
+                alphabetALine1.add(new dots(i, j));
+            }
+            for(float i = 35/10*width ; i <= 65/10*width ; i+=80){
+                    j = 78366 / 100;
+                    alphabetALine1.add(new dots(i, j));
+            }
+        }
+
+        public void updatePath(float x , float y){ // update The pathList
+            pathList.add(new dots(x,y)) ;
+        }
+
+        public void print(){
+            System.out.println(vitalPoints.get(0).getCoordinateX()+" vital0 " + vitalPoints.get(0).getCoordinateY());
+            System.out.println(lastXCoordinate + " last " + lastYCoordinate);
+            System.out.println(alphabetALine1.get(2).getCoordinateX() + " huhu2 " + alphabetALine1.get(2).getCoordinateY()) ;
+            System.out.println(alphabetALine1.get(10).getCoordinateX() + " huhu10 " + alphabetALine1.get(10).getCoordinateY()) ;
+            if(pathList.size() > 0)
+                System.out.println(pathList.get(0).getCoordinateX() + " pathList " + pathList.get(0).getCoordinateY());
+        }
+
     }
-}
