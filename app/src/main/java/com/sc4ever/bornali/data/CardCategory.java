4925468bbc207c6454b419cda5f732b4768c0647
Asfaqur Rahman <abirftw@gmail.com>
2019-11-12 22:40:39 +0600

    Add unary recursive relation for the database Remove redundant data model class

--- app/src/main/java/com/sc4ever/bornali/data/CardCategory.java
+++ app/src/main/java/com/sc4ever/bornali/data/CardCategory.java
@@ -2,23 +2,63 @@ package com.sc4ever.bornali.data;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 @Entity(
-        tableName = "card_categories"
+        tableName = "card_categories",
+        foreignKeys = @ForeignKey(entity = CardCategory.class, parentColumns = "cat_id",
+                childColumns = "part_of_id", onDelete = ForeignKey.CASCADE
+        )
 )
 public class CardCategory {
-     @PrimaryKey(autoGenerate = true)
-     @ColumnInfo(name = "cat_id", index = true)
+     @PrimaryKey
+     @ColumnInfo(name = "cat_id")
      private int ID;
      @ColumnInfo (name = "cat_desc")
      private String text;
      @ColumnInfo (name = "cat_uri")
      private String imgURI;
+     @ColumnInfo(name = "part_of_id", index = true)
+     private int partOFID;
+     private static int cardCount = 0;
 
-     public CardCategory(int ID, String text, String imgURI) {
-          this.ID = ID;
-          this.text = text;
-          this.imgURI = imgURI;
-     }
+     static int getCardCount() {
+        return cardCount;
+    }
+     static void increment(){
+        ++cardCount;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImgURI() {
+        return imgURI;
+    }
+
+    void setImgURI(String imgURI) {
+        this.imgURI = imgURI;
+    }
+
+    public int getPartOFID() {
+        return partOFID;
+    }
+
+    void setPartOFID(int partOFID) {
+        this.partOFID = partOFID;
+    }
 }
